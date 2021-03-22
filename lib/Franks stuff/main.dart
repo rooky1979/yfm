353dad7e507a53bd2c1281acb1b88c2c5280a936
Michael Rust <51839640+Mrustnz@users.noreply.github.com>
2021-03-22 15:21:11 +1300

    added new file

---
+++ lib/Franks stuff/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'YFMHomePage.dart';
+
+MaterialApp newMethod() {
+  return new MaterialApp(
+    home: YFMHomePage(),
+  );
+}
+
+void main() {
+  runApp(new MaterialApp(
+    home: YFMHomePage(),
+  ));
+}
