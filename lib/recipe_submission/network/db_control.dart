e09380120bdda1aded13b23dc6d7d3674a456f73
Matthew Rook <pwm1057@autuni.ac.nz>
2021-04-10 11:03:54 +0100

    db control method created, variables updated

---
+++ lib/recipe_submission/network/db_control.dart
@@ -0,0 +1,60 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class DBControl {
+  //recipe_info
+  //value used for dropdown selection in dropdown menus
+  static var categoryValue; //
+  static var difficultyValue;
+  //value used for the checkboxes
+  static List allergies; //
+  static List proteins; //
+  //text controllers for the textfields
+  static TextEditingController recipeNameController; //
+  static TextEditingController servingsController; //
+  static TextEditingController hoursController;
+  static TextEditingController minutesController;
+  static TextEditingController descriptionController; //
+  static int prepTime;
+
+  //list to hold ingredient strings
+  static List ingredients = []; //
+
+  //recipe_method
+  //list to hold ingredient strings
+  static List methodSteps = [];
+
+  //recipe_image
+  static File image;
+  //something in here to change the
+  //string name or apply the recipe doc name ID???
+
+  //declare and instantiate the firebase storage bucket
+  final FirebaseStorage storage = FirebaseStorage.instanceFor(
+      bucket: 'gs://youth-food-movement.appspot.com');
+  //firebase instance
+  static var firestoreDb =
+      FirebaseFirestore.instance.collection('recipe').snapshots();
+
+  static void writeDB() async {
+    var recipeDocRef =
+        await FirebaseFirestore.instance.collection('recipe').add({});
+
+    await recipeDocRef.collection("ingredients").add({
+      "difficulty": difficultyValue,
+      "allergies": allergies,
+      "category": categoryValue,
+      "description": descriptionController.text,
+      "image": "testing",
+      "ingredients": ingredients,
+      "prepTime": prepTime,
+      "protein": proteins,
+      "servings": servingsController.text,
+      "title": recipeNameController.text
+    });
+    await recipeDocRef.collection("method").add({"method": methodSteps});
+  }
+
+}
