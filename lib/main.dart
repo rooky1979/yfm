34e865ad1024c1a706e61fbc6ef7026e0a275683
98mbohra <98mbohra@gmail.com>
2021-04-12 12:01:05 +1200

    Working

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,21 @@ import 'login/login_page.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(new MaterialApp(
+  runApp(
+    new MaterialApp(
       //theme: _appTheme,
       theme: ThemeData(
-    //brightness: Brightness.light,
-    primaryColor: Colors.red[300],
-    textTheme: TextTheme(
-      headline5: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
-      bodyText1: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
-      bodyText2: TextStyle(
-        fontSize: 25,
-        color: Colors.white,
+        //brightness: Brightness.light,
+        primaryColor: Colors.red[300],
+        /* textTheme: TextTheme(
+
+            headline5: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
+            bodyText1: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
+            bodyText2: TextStyle(
+                fontSize: 25,
+                color: Colors.white,*/
       ),
       home: LoginPage(),
     ),
-  )));
+  );
 }
