e1c408f23b2c625db8709439242c509edea238dd
nmz1671 <nmz1671@autuni.ac.nz>
2021-04-22 11:19:17 +1200

    updating

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'login/login_page.dart';
+import 'package:youth_food_movement/homepage/HomePage.dart';
+import 'package:youth_food_movement/homepage/profile_page.dart';
+import 'package:youth_food_movement/homepage/settings_page.dart';
+import 'package:youth_food_movement/recipe/ui/ingredients_page.dart';
+//import 'package:youth_food_movement/recipe/ui/test_homepage.dart';
+import 'package:youth_food_movement/recipe_submission/ui/recipe_submit_info.dart';
+import 'package:youth_food_movement/recipe_submission/ui/recipe_submit_ingredients.dart';
+import 'package:youth_food_movement/login/login_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(new MaterialApp(
-    //theme: _appTheme,
-    theme: ThemeData(
-      //brightness: Brightness.light,
-      primaryColor: Colors.red[300],
-      /* textTheme: TextTheme(
+  runApp(
+    new MaterialApp(
+      //theme: _appTheme,
+      theme: ThemeData(
+        //brightness: Brightness.light,
+        primaryColor: Colors.red[300],
+        /* textTheme: TextTheme(
+
             headline5: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
             bodyText1: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
             bodyText2: TextStyle(
                 fontSize: 25,
-                color: Colors.white,
-
-                fontWeight: FontWeight.bold)) */),
-    home: LoginPage(),
-  ),
+                color: Colors.white,*/
+      ),
+      //home: LoginPage(),
+      home: LoginPage(),
+    ),
   );
-
 }
