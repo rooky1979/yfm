3fe6adabd0385099d60efd7853b4070d88f1d9d4
Michael Rust <51839640+Mrustnz@users.noreply.github.com>
2021-05-21 16:12:46 +1200

    ;0

--- lib/homepage/home_page.dart
+++ lib/homepage/home_page.dart
@@ -2,7 +2,6 @@ import 'dart:ui';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:youth_food_movement/colours/hex_colours.dart';
 import 'package:youth_food_movement/homepage/profile_page.dart';
