026ec2c53b2634b38692230b7c52b8de6014c63b
98mbohra <49011759+98mbohra@users.noreply.github.com>
2021-05-23 03:26:26 +1200

    main removed imports

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:youth_food_movement/colours/hex_colours.dart';
 import 'package:youth_food_movement/login/login_page.dart';
-import 'package:youth_food_movement/login/user_detail_page.dart';
-import 'package:youth_food_movement/login/user_info.dart';
-import 'package:youth_food_movement/recipe_submission/ui/recipe_submit_success.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
